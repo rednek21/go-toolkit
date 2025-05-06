41952ea716669968334f0af512e22526f0f9ba50
rednek21 <egor.podustov@mail.ru>
2025-05-06 18:00:42 +0300

    update errors pkg

--- errors/errors.go
+++ errors/errors.go
@@ -1,6 +1,7 @@
 package errors
 
 import (
+	"errors"
 	"fmt"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -13,6 +14,7 @@ const (
 	ErrCodeAlreadyExists    ErrCode = "ALREADY_EXISTS"
 	ErrCodeInvalidInput     ErrCode = "INVALID_INPUT"
 	ErrCodeInternal         ErrCode = "INTERNAL_ERROR"
+	ErrCodeInvalidArgument  ErrCode = "INVALID_ARGUMENT"
 	ErrCodeUnauthorized     ErrCode = "UNAUTHORIZED"
 	ErrCodePermissionDenied ErrCode = "PERMISSION_DENIED"
 )
@@ -21,6 +23,7 @@ type ErrorImpl struct {
 	Code    ErrCode `json:"code"`
 	Message string  `json:"message"`
 	Err     error   `json:"-"`
+	Details any     `json:"details,omitempty"`
 }
 
 func New(code ErrCode, message string, err error) *ErrorImpl {
@@ -42,17 +45,22 @@ func (e *ErrorImpl) Unwrap() error {
 	return e.Err
 }
 
-func (e *ErrorImpl) GRPCStatus() *status.Status {
-	return status.New(e.toGrpcCode(), e.Message)
+func (e *ErrorImpl) WithDetails(details any) *ErrorImpl {
+	e.Details = details
+	return e
 }
 
-func (e *ErrorImpl) toGrpcCode() codes.Code {
+func (e *ErrorImpl) GRPCStatus() *status.Status {
+	return status.New(e.toGRPCCode(), e.Message)
+}
+
+func (e *ErrorImpl) toGRPCCode() codes.Code {
 	switch e.Code {
 	case ErrCodeNotFound:
 		return codes.NotFound
 	case ErrCodeAlreadyExists:
 		return codes.AlreadyExists
-	case ErrCodeInvalidInput:
+	case ErrCodeInvalidInput, ErrCodeInvalidArgument:
 		return codes.InvalidArgument
 	case ErrCodeUnauthorized:
 		return codes.Unauthenticated
@@ -61,4 +69,20 @@ func (e *ErrorImpl) toGrpcCode() codes.Code {
 	default:
 		return codes.Internal
 	}
+}
+
+func (e *ErrorImpl) Is(target error) bool {
+	t, ok := target.(*ErrorImpl)
+	if !ok {
+		return false
+	}
+	return e.Code == t.Code
+}
+
+func AsErrCode(err error) (ErrCode, bool) {
+	var e *ErrorImpl
+	if errors.As(err, &e) {
+		return e.Code, true
+	}
+	return "", false
 }
